a8d67b04eea96680999f9d3a06a4685994ae54f0
MarcoSolarino <marcosolarino@live.it>
2021-11-03 10:53:32 +0100

    - get_sequences(): get dictionary of sequences - generate_graph(): generate graph from sequences - find_edges(): given sequences find all references between them in comments - filter_id(): given a number n return true if it is an id

--- main.py
+++ main.py
@@ -1,25 +1,59 @@
 import os
 import json
 import networkx as nx
+import re
+from collections import Counter
 
 
 path = 'sequences/sequences/'
 
-# get all sequences names
-filenames = [filename for filename in os.listdir(path)]
-num_seq = len(filenames)
 
-# build sequences dictionaries
-sequences = []
-for f in filenames:
-    with open(path + f) as json_file:
-        sequences.append(json.load(json_file))
+def get_sequences():
+    # get all sequences names
+    filenames = [filename for filename in os.listdir(path)]
 
-print(f'len dictionary: {len(sequences)}')
-print(f'id: {((sequences[0])["query"]).split(":")[1]}')
-print(f'comment: {((sequences[0])["results"][0])["comment"]}')
+    # build sequences dictionaries
+    sequences = []
+    for f in filenames:
+        with open(path + f) as json_file:
+            sequences.append(json.load(json_file))
+    return sequences
+
+
+def generate_graph():
+    sequences = get_sequences()
+    graph = nx.Graph()
+
+    ids = [(s["query"]).split(":")[1] for s in sequences]  # get all ids
+    graph.add_nodes_from(ids)  # add all nodes at once
+
+    edges = find_edges(sequences)
+    counter = Counter(edges)
+    graph.add_edges_from(edges)  # add all edges at once
+    return graph
+
+
+def find_edges(sequences):
+    edges = []
+    for s in sequences:
+        id = (s["query"]).split(":")[1]
+        if 'comment' in s['results'][0]:
+            comments = (s["results"][0])["comment"]
+            for c in comments:
+                numbers_in_comment = re.findall('[0-9]+', c)
+                numbers_in_comment = list(filter(filter_id, numbers_in_comment))
+                for n in numbers_in_comment:
+                    edges.append((id, 'A' + str(n)))
+    return edges
+
+
+def filter_id(n):
+    if len(n) == 6:
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
-    pass
-
+    g = generate_graph()
+    print(f'graph nodes = {g.number_of_nodes()}  graph edges = {g.number_of_edges()}')
